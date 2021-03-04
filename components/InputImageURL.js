8e01ae645e7dba0af14522efc8c21e465175ef65
Sonu Sharma <echosonusharma@gmail.com>
2021-03-05 01:00:53 +0530

    information and style changed

--- components/InputImageURL.js
+++ components/InputImageURL.js
@@ -3,7 +3,7 @@ const InputImage = ({ setSearchInput, buttonDetect }) => {
     return (
         <div className="flex justify-center p-24 ">
             <input
-                type="image"
+                type="text"
                 placeholder="Enter image Link"
                 className="w-2/4 border-2 border-black p-1 mr-4 px-6 placeholder-gray-700 placeholder-opacity-75 "
                 onChange={e => setSearchInput(e.target.value)} />
